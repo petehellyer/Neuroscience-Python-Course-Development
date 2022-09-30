b63a5907197a0f1ddf698ce832f823ad1e03146e
valegiunchiglia <giunchiglia.valentina@gmail.com>
2022-09-30 22:37:34 +0100

    finals

--- first_script.py
+++ first_script.py
@@ -10,5 +10,6 @@ def mean_script(var1, var2, var3): #Name of the function
     var3 (int): value 3
     '''
     sum_vars = var1+var2+var3 #code of the function
+    print(sum_vars)
     mean = sum_vars/3 #code of the function
     return mean #output
