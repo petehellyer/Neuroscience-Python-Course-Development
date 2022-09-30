3e8e6428daf1cb4a7076203f253b509df99ce422
valegiunchiglia <giunchiglia.valentina@gmail.com>
2022-09-30 19:05:12 +0100

    finals

---
+++ mean.py
@@ -0,0 +1,14 @@
+
+
+def mean(var1, var2, var3): #Name of the function
+    '''
+    The aim of the function is to calculate the mean
+    of three given input variables.
+    Example Parameters:
+    var1 (int): value 1
+    var2 (int): value 2
+    var3 (int): value 3
+    '''
+    sum_vars = var1+var2+var3 #code of the function
+    mean = sum_vars/3 #code of the function
+    return mean #output
